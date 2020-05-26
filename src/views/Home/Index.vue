58f057cbc07dd04e7355794ecd51c9b0f3aef683
Esteban Estivill <eestivill73@gmail.com>
2020-05-26 09:31:41 +0200

    Este es mi primir commit

---
+++ src/views/Home/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home-view">
+    <HomeTitle/>
+    <MainForm/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MainForm from './HomeForm'
+import HomeTitle from './HomeTitle'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HomeTitle,
+    MainForm
+  }
+}
+</script>
